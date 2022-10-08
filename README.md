d6e2a83bcd3cef1d98a0d4cbaa0b30d1857b45ab
Rodrigo A. Lima <rodrigoalveslima@gmail.com>
2022-10-08 21:38:57 +0000

    Release v1.0.

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
 # ATLoad
+ATLoad is a Python library for building synthetic workload generators. It
+enables the reproducible generation of bursty workloads that are representative
+of online services.
+
+* ATLoad clients can be configured to use a closed or open loop for sending
+requests. When using a closed loop, a client can only send a new request after
+its last request has returned.
+* ATLoad clients can be configured to use a specific statistical distribution
+(e.g., Poisson, uniform, or constant) for generating think times (i.e., time
+intervals between consecutive requests).
+* ATLoad clients can be configured to inject burstiness in the workload using a
+*Markovian Arrival Process (MAP)*, as described in this
+[paper](https://dl.acm.org/doi/abs/10.1145/1555228.1555267).
+* ATLoad clients generate a request mix using a configurable graph where nodes
+represent request types and arc weights represent probabilities of transitioning
+between request types.
+
+## Workload Configuration
+This is the configuration for a bursty workload that simulates 200 users sending
+3 types of requests (`write`, `read`, and `delete`) for 300 seconds. The mean
+think time is 10 seconds.
+```
+sessions: 200                     # number of concurrent client sessions
+loop: closed                      # type of client loop ("closed" or "open")
+duration:
+  total: 300                      # total duration in seconds
+  ramp_up: 60                     # ramp up time in seconds
+  ramp_down: 60                   # ramp down time in seconds
+think_time: 10                    # mean think time in seconds
+think_time_distribution: poisson  # distribution of client think time
+                                  # ("poisson", "uniform", or "constant")
+burstiness:
+  window: 1.0                     # burstiness window in seconds
+  intensity: 4                    # burstiness intensity (as a multiplier of
+                                  # average workload)
+  turn_on_prob: 0.2               # probability of turning bursty mode on
+  turn_off_prob: 0.1              # probability of turning bursty mode off
+request_graph:
+  main:
+    write: 1.0                    # probability of writing first
+    read: 0                       # probability of reading first
+    delete: 0                     # probability of deleting first
+  write:
+    read: .5                      # probability of reading after writing
+    delete: .5                    # probability of deleting after writing
+  read:
+    read: .1                      # probability of reading after reading
+    delete: .9                    # probability of deleting after reading
+  delete:
+    read: .3                      # probability of reading after deleting
+    delete: .7                    # probability of deleting after deleting
+```
+
+## Session Implementation (Python 3)
+Requests are sent by client sessions that simulate real users. Each client
+session is an instance of a class inherited from `ATLoad.Session` and runs on
+its own thread. Each request type is implemented by a method with the same name
+in that class.
+
+This class `Session` is a skeleton for the implementation of those 3 request
+types (`write`, `read`, and `delete`):
+```
+import argparse
+
+import ATLoad
+
+
+class Session(ATLoad.Session):
+  def write(self):
+    # Implement write.
+    pass
+
+  def read(self):
+    # Implement read.
+    pass
+
+  def delete(self):
+    # Implement delete.
+    pass
+
+
+if __name__ == "__main__":
+  # Parse command-line arguments.
+  parser = argparse.ArgumentParser(description="Generate a workload")
+  parser.add_argument("--workload_conf", required=True, action="store",
+      type=str, help="Path to the workload configuration file")
+  parser.add_argument("--log", required=True, action="store", type=str,
+      help="Path to the log file")
+  parser.add_argument("--seed", required=True, action="store", type=str,
+      help="Random number generator seed")
+  parser.add_argument("--n_workers", required=True, action="store", type=str,
+      help="Number of worker processes")
+  args = parser.parse_args()
+  # Generate workload.
+  workload = ATLoad.Workload(args.workload_conf, args.log, Session,
+      int(args.seed), int(args.n_workers))
+  workload.run()
+```
+
+## Developer
+- Rodrigo Alves Lima (ral@gatech.edu)
+
+## License
+Copyright (C) 2022 Georgia Tech Center for Experimental Research in Computer
+Systems.
+
+Licensed under the Apache License 2.0.
---
+++ src/ATLoad.py
@@ -0,0 +1,139 @@
+# Copyright (C) 2022 Georgia Tech Center for Experimental Research in Computer Systems
+
+import collections
+import multiprocessing
+import os
+import random
+import threading
+import time
+
+import numpy
+import yaml
+
+
+class Session:
+
+  def _run(self, conf, start_time, start_at, stop_at, request_graph, mean_think_time, think_time_generator, intensity):
+    self._logs = []
+    self._request_graph = request_graph
+    # Wait to start.
+    while time.time() < start_at:
+      time.sleep(0.01)
+    # Session loop.
+    request = "main"
+    while time.time() < stop_at:
+      request = self._select_next_request(request)
+      request_thread = threading.Thread(target=getattr(self, request), daemon=True)
+      request_thread.start()
+      think_time = think_time_generator(mean_think_time)
+      while think_time > 0:
+        time.sleep(0.01)
+        think_time -= 0.01
+        if isinstance(intensity, list):
+          elapsed = time.time() - start_time
+          window_no = int(elapsed / conf["burstiness"]["window"])
+          if intensity[window_no] != 1:
+            window_start = window_no * conf["burstiness"]["window"]
+            window_end = window_start + conf["burstiness"]["window"]
+            window_half = (window_start + window_end) / 2
+            if (window_no == 0 or intensity[window_no - 1] == 1) and elapsed < window_half:
+              think_time -= 0.01 * (intensity[window_no] - 1) * ((elapsed - window_start) / (conf["burstiness"]["window"] / 2))
+            elif intensity[window_no + 1] == 1 and elapsed > window_half:
+              think_time -= 0.01 * (intensity[window_no] - 1) * ((window_end - elapsed) / (conf["burstiness"]["window"] / 2))
+            else:
+              think_time -= 0.01 * (intensity[window_no] - 1)
+      if conf["loop"] == "closed":
+        request_thread.join()
+
+  def _select_next_request(self, request):
+    r = random.uniform(0, sum(self._request_graph[request].values()))
+    for request, weight in self._request_graph[request].items():
+      if r < weight:
+        return request
+      r -= weight
+
+  def _log(self, message):
+    self._logs.append((time.time(), message))
+
+
+class Workload:
+
+  def __init__(self, conf_filename, log_filename, session_cls, random_seed, n_workers, *args):
+    # Initialize the random number generator.
+    random.seed(random_seed)
+    # Parse configuration file and copy parameters.
+    with open(conf_filename) as conf_file:
+      self._conf = yaml.safe_load(conf_file)
+    self._log_filename = log_filename
+    self._session_cls = session_cls
+    self._n_workers = n_workers
+    self._args = args
+    self._n_sessions = self._conf["sessions"]
+    self._duration = self._conf["duration"]
+    self._request_graph = dict([(request, collections.OrderedDict(self._conf["request_graph"][request])) for request in self._conf["request_graph"]])
+    self._mean_think_time = self._conf["think_time"]
+    self._think_time_generator = getattr(numpy.random,
+                                         self._conf["think_time_distribution"]) if self._conf["think_time_distribution"] != "constant" else lambda x: x
+    if "burstiness" not in self._conf:
+      self._intensity = 1
+    else:
+      self._intensity = []
+      for i in range(int((self._conf["duration"]["total"] + 60) / self._conf["burstiness"]["window"])):
+        if i * self._conf["burstiness"]["window"] > self._conf["duration"]["ramp_up"] and \
+            i * self._conf["burstiness"]["window"] < self._conf["duration"]["total"] - self._conf["duration"]["ramp_down"]:
+          if i == 0 or self._intensity[-1] != 1:
+            self._intensity.append(self._conf["burstiness"]["intensity"] if random.uniform(0, 1) > self._conf["burstiness"]["turn_off_prob"] else 1)
+          else:
+            self._intensity.append(self._conf["burstiness"]["intensity"] if random.uniform(0, 1) < self._conf["burstiness"]["turn_on_prob"] else 1)
+        else:
+          self._intensity.append(1)
+
+  def _run_worker(self, log_filename, start_time, n_sessions, start_at_delta):
+    # Initialize sessions.
+    sessions = [self._session_cls(*self._args) for i in range(n_sessions)]
+    # Run each session in its own thread.
+    threads = [
+        threading.Thread(target=session._run,
+                         args=[
+                             self._conf, start_time, start_time + self._duration["ramp_up"] * (i / n_sessions) + start_at_delta,
+                             start_time + self._duration["total"] - self._duration["ramp_down"] * (i / n_sessions), self._request_graph, self._mean_think_time,
+                             self._think_time_generator, self._intensity
+                         ]) for (i, session) in enumerate(sessions)
+    ]
+    for thread in threads:
+      thread.start()
+    # Wait until all sessions are finished.
+    for thread in threads:
+      thread.join()
+    # Arrange session logs in chronological order.
+    logs = []
+    while True:
+      session_i = None
+      for (i, session) in enumerate(sessions):
+        if session._logs and (session_i is None or session._logs[0][0] < sessions[session_i]._logs[0][0]):
+          session_i = i
+      if session_i is None:
+        break
+      logs.append(sessions[session_i]._logs.pop(0)[1])
+    # Write logs to a file.
+    with open(log_filename, 'w') as log_file:
+      for (i, log) in enumerate(logs):
+        if i:
+          log_file.write("\n")
+        log_file.write(log)
+
+  def run(self):
+    start_time = time.time()
+    dirname = os.path.dirname(self._log_filename)
+    filename, extension = os.path.basename(self._log_filename).split('.')
+    workers = [
+        multiprocessing.Process(target=self._run_worker,
+                                args=(os.path.join(dirname, filename + str(i) + '.' + extension), start_time, int(self._n_sessions // self._n_workers),
+                                      i * self._conf["duration"]["ramp_up"] / self._n_sessions)) for i in range(self._n_workers)
+    ]
+    # Run each worker in its own process.
+    for worker in workers:
+      worker.start()
+    # Wait until all workers are finished.
+    for worker in workers:
+      worker.join()
